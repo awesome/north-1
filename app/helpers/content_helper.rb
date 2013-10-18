03061c18ab58d82cb2dd785c70034b01d2c61eef
Manu S Ajith <neo@codingarena.in>
2013-10-19 05:10:59 +0530

    changed title and meta links for better SEO

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -3,9 +3,9 @@ module ContentHelper
     links = Array.new
     args.each do |item|
       if "/#{params[:path].to_s}" == item[1]
-        links << link_to(item[0], item[1], {:class=>'active'})
+        links << link_to(item[0], item[1], {:class=>'active', :title => item[2]})
       else
-        links << link_to(item[0], item[1])
+        links << link_to(item[0], item[1], {:title => item[2] })
       end
     end
     content_tag :div, links.join(' | '), :class=>'header_links'
